f83b35fead537dbd1542d42fb25c83af568e0aa6
Yanny Patel <47578696+Yanny24211@users.noreply.github.com>
2023-10-05 16:55:17 -0400

    created lesson1 for js basics

---
+++ lesson1.js
@@ -0,0 +1,15 @@
+console.log(23 + 97); 
+let result = 23 + 91 + 12 + 14 + 81 + 17; 
+console.log(result); 
+
+let equation = (4+6+9)/77;
+console.log(equation); 
+
+let a = 10
+console.log(a); 
+console.log(9*a); 
+let b = 7*a;
+console.log(b); 
+
+const MAX = 57; 
+const actual = MAX - 13; 
